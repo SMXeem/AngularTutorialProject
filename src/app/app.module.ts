52e2e0479c9f255e82266612f58db46c1e49990d
Sayed Al Zawad XEEM <sayedxeem24@gmail.com>
2020-11-19 01:17:34 +0600

    PropertyBinding in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
