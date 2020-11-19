013d5ed3ff1f3daf79ff9d4c102e97fd2dc2fdf6
Sayed Al Zawad XEEM <sayedxeem24@gmail.com>
2020-11-19 16:08:03 +0600

    Service using in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import { NgSwitchDirectiveComponent } from './ng-switch-directive/ng-switch-dire
 import { NgForDirectiveComponent } from './ng-for-directive/ng-for-directive.component';
 import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
 import { PipesComponent } from './pipes/pipes.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
@@ -31,14 +34,16 @@ import { PipesComponent } from './pipes/pipes.component';
     NgSwitchDirectiveComponent,
     NgForDirectiveComponent,
     ComponentInteractionComponent,
-    PipesComponent
+    PipesComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
