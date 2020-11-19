e142a3d956aa60ea5ea1c3c9397c5b1a97a4d5d6
Sayed Al Zawad XEEM <sayedxeem24@gmail.com>
2020-11-20 02:57:23 +0600

    RoutingAndNavigation in Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepertmentComponent } from './depertment/depertment.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/depertments', pathMatch: 'full'},
+  { path: 'depertments', component: DepertmentComponent},
+  { path: 'employees', component: EmployeeListComponent},
+  {path: '**', component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[DepertmentComponent, EmployeeListComponent]
